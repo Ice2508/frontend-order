1e2dd7be042e679657f403575655566a97c4f4c4
Ice2508 <Ice2508@yandex.ru>
2026-02-12 13:04:17 +0300

    ok

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const id = Number(document.querySelector('.product-card').dataset.id);
 
 productBtn.addEventListener('click', async (e) => {
     e.preventDefault();
-    const response = await fetch('https://dedicated-sparkle-ca5141ccb3.strapiapp.com/api/orders', {
+    const response = await fetch('https://dedicated-sparkle-ca5141ccb3.strapiapp.com/api/order', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
